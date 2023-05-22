fa40b471364738b957807927dd26ac21fdc91c5e
EmanuelCN <emanuelghub@gmail.com>
2023-05-22 22:59:03 +0000

    fs: Fix declaration after statement warning
    warning: mixing declarations and code is a C99 extension [-Wdeclaration-after-statement]
            const struct cred *old_cred;

--- fs/open.c
+++ fs/open.c
@@ -362,6 +362,7 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+        ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
